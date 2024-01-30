c370debfd70bc3285a19f03170963ed37592f625
Roongkun <roongkun.l@gmail.com>
2024-01-30 13:06:15 +0700

    implemented crud operation interface

--- src/backend/internal/model/model.go
+++ src/backend/internal/model/model.go
@@ -1,4 +1,15 @@
 package model
 
+import (
+	"github.com/google/uuid"
+	"github.com/uptrace/bun"
+)
+
 type User struct {
+	bun.BaseModel  `bun:"table:users, alias:u"`
+	Id             uuid.UUID `bun:"id,pk,type:uuid,default:gen_random_uuid()" json:"id"`
+	Name           string    `bun:"name, type:varchar" json:"name"`
+	Email          *string   `bun:"email,type:varchar" json:"email"`
+	Password       *string   `bun:"password, type:varchar" json:"password"`
+	IsPhotographer bool      `bun:"is_photographer, type:boolean" json:"is_photographer"`
 }
