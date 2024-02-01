5f786115512641474c3256e3ada34ecdb518c6b2
Roongkun <roongkun.l@gmail.com>
2024-02-01 17:02:21 +0700

    fixed scripts

--- src/backend/scripts/initdb.sh
+++ src/backend/scripts/initdb.sh
@@ -1,5 +1,1 @@
-until [ "`docker inspect -f {{.State.Running}} postgresql`"=="true" ]; do
-    sleep 0.1;
-done;
-
-go run main.go migrate up -c ./internal/config/values.yaml
+while [ "`docker inspect -f {{.State.Running}} postgresql`" != "true" ]; do     sleep 2; done
