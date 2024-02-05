d630feeb45842a26e2fe84c6f9a96c0e72926499
Papanin Kittawisinpoon <bampapanin2003@gmail.com>
2024-02-05 16:13:34 +0700

    untest

--- src/backend/internal/controller/user/fieldvalidate/user.go
+++ src/backend/internal/controller/user/fieldvalidate/user.go
@@ -3,6 +3,8 @@ package fieldvalidate
 import (
 	"errors"
 
+	"regexp"
+
 	"github.com/Roongkun/software-eng-ii/internal/model"
 )
 
@@ -16,6 +18,41 @@ func LoginUser(cred model.LoginCredentials) []error {
 		))
 	}
 	if cred.Password == "" {
+		fieldErrs = append(fieldErrs, errors.New(
+			"password must be specified",
+		))
+	}
+
+	return fieldErrs
+}
+
+func RegCustomer(newUser model.UserInput) []error {
+	fieldErrs := []error{}
+
+	// empty name
+	if newUser.Name == "" {
+		fieldErrs = append(fieldErrs, errors.New(
+			"Name must be specified",
+		))
+	}
+
+	// empty email
+	if newUser.Email == "" {
+		fieldErrs = append(fieldErrs, errors.New(
+			"email must be specified",
+		))
+	} else {
+		// email format
+		emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+		if emailRegex.MatchString(newUser.Email) == false {
+			fieldErrs = append(fieldErrs, errors.New(
+				"invalid email address",
+			))
+		}
+	}
+
+	// empty password
+	if newUser.Password != nil || *newUser.Password == "" {
 		fieldErrs = append(fieldErrs, errors.New(
 			"password must be specified",
 		))
---
+++ src/backend/internal/controller/user/reg_customer.go
@@ -0,0 +1,50 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/Roongkun/software-eng-ii/internal/controller/user/fieldvalidate"
+	"github.com/Roongkun/software-eng-ii/internal/controller/util"
+	"github.com/Roongkun/software-eng-ii/internal/model"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func (r *Resolver) RegCustomer(c *gin.Context) {
+	newUser := model.UserInput{}
+	if err := c.BindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   err.Error(),
+			"message": "unable to bind request body with json, please recheck",
+		})
+		c.Abort()
+		return
+	}
+
+	// field validate
+	if fieldErr := fieldvalidate.RegCustomer(newUser); len(fieldErr) > 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": util.JSONErrs(fieldErr),
+		})
+		c.Abort()
+		return
+	}
+
+	// change type
+	userModel := model.User{
+		Id:        uuid.New(),
+		Name:      newUser.Name,
+		Email:     newUser.Email,
+		Password:  newUser.Password,
+		LoggedOut: false,
+	}
+
+	// add to database
+	if err := r.UserUsecase.UserRepo.AddOne(c, &userModel); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		c.Abort()
+		return
+	}
+}
