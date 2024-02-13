31a8db59df37155bb119a4c2773779cd0eaebe01
Maekin <74455990+mekintown@users.noreply.github.com>
2024-02-13 15:33:09 +0700

    add edit profile page

--- src/frontend/src/types.ts
+++ src/frontend/src/types.ts
@@ -1,10 +1,16 @@
-interface NewUser {
+export interface NewUser {
   name: string;
   email: string;
   password: string;
 }
 
-interface LoginCredentials {
+export interface LoginCredentials {
   email: string;
   password: string;
+}
+
+export enum Gender {
+  Male = "Male",
+  Female = "Female",
+  Other = "Other",
 }
