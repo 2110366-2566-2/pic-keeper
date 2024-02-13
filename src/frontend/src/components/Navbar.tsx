684b144178341c74700925f8b7a79388e5889423
Maekin <74455990+mekintown@users.noreply.github.com>
2024-02-13 19:39:11 +0700

    add dummy for my profile page

--- src/frontend/src/components/Navbar.tsx
+++ src/frontend/src/components/Navbar.tsx
@@ -132,7 +132,7 @@ const NavBar = () => {
                             <Menu.Item>
                               {({ active }) => (
                                 <a
-                                  href="/my-profile"
+                                  href="user/my-profile"
                                   className={classNames(
                                     active ? "bg-gray-100" : "",
                                     "block px-4 py-2 text-sm text-gray-700"
