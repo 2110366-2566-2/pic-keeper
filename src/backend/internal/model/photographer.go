7f5e3666a8e72011076ef6cb1d76f56b6c09b89c
Roongkun <roongkun.l@gmail.com>
2024-02-14 23:34:01 +0700

    remove deprecated model

--- src/backend/internal/model/photographer.go
+++
@@ -1,9 +0,0 @@
-package model
-
-// RegisterPhotographerInput defines the data needed for registering a photographer.
-type RegisterPhotographerInput struct {
-	Name       string `json:"name" binding:"required"`
-	Email      string `json:"email" binding:"required,email"`
-	Password   string `json:"password" binding:"required"`
-	IsVerified bool   `json:"is_verified"`
-}
