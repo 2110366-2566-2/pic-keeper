731223aeccd4cf62f5b4db341295fd79d7fcf89e
Roongkun <roongkun.l@gmail.com>
2024-02-23 10:44:06 +0700

    finished io

--- src/backend/internal/controller/socketio/io.go
+++ src/backend/internal/controller/socketio/io.go
@@ -60,3 +60,73 @@ func (io *IO[T]) Error(socketId uuid.UUID, err error) bool {
 
 	return socket.Error(NewError(err.Error()))
 }
+
+func (io *IO[T]) Emit(socketId uuid.UUID, msg T) bool {
+	io.mu.RLock()
+	socket, ok := io.sockets[socketId]
+	io.mu.RUnlock()
+
+	if !ok {
+		return false
+	}
+
+	return socket.Emit(msg)
+}
+
+func (io *IO[T]) EmitAny(socketId uuid.UUID, msg any) bool {
+	io.mu.RLock()
+	socket, ok := io.sockets[socketId]
+	io.mu.RUnlock()
+
+	if !ok {
+		return false
+	}
+
+	return socket.EmitAny(msg)
+}
+
+func (io *IO[T]) BroadcastAny(msg any) (total int, sent int) {
+	io.mu.Lock()
+	total = len(io.sockets)
+	io.mu.Unlock()
+
+	for _, socket := range io.sockets {
+		if socket.EmitAny(msg) {
+			sent++
+		}
+	}
+
+	return
+}
+
+func (io *IO[T]) Broadcast(msg T) (total int, sent int) {
+	io.mu.Lock()
+	total = len(io.sockets)
+	io.mu.Unlock()
+
+	for _, socket := range io.sockets {
+		if socket.Emit(msg) {
+			sent++
+		}
+	}
+
+	return
+}
+
+func (io *IO[T]) register(socket *Socket[T]) {
+	io.mu.Lock()
+	io.sockets[socket.ID] = socket
+	io.mu.Unlock()
+}
+
+func (io *IO[T]) deregister(socket *Socket[T]) {
+	io.mu.Lock()
+
+	socket, ok := io.sockets[socket.ID]
+	if ok {
+		socket.close()
+		delete(io.sockets, socket.ID)
+	}
+
+	io.mu.Unlock()
+}
