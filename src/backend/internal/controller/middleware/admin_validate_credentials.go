01763b35b9a64699d132a9d654f4fe58f4c7ab44
Roongkun <roongkun.l@gmail.com>
2024-02-27 10:27:58 +0700

    changes done

--- src/backend/internal/controller/middleware/admin_validate_credentials.go
+++ src/backend/internal/controller/middleware/admin_validate_credentials.go
@@ -34,11 +34,11 @@ func ValidateCredentials(c *gin.Context) {
 		Issuer:    "AuthProvider",
 	}
 
-	claims, err := jwtWrapper.ValidateToken(token, true)
+	claims, err := jwtWrapper.ValidateToken(c, token, true)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
+		c.JSON(c.GetInt("errorStatus"), gin.H{
 			"status": "failed",
-			"error":  err.Error(),
+			"error":  c.GetString("errorMessage"),
 		})
 		c.Abort()
 		return
--- src/backend/internal/third-party/auth/jwt.go
+++ src/backend/internal/third-party/auth/jwt.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/Roongkun/software-eng-ii/internal/third-party/databases"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
 
@@ -46,7 +48,7 @@ func (j *JwtWrapper) GenerateToken(ctx context.Context, email string, isAdmin bo
 	return signedToken, nil
 }
 
-func (j *JwtWrapper) ValidateToken(signedToken string, isAdmin bool) (claims *JwtClaim, err error) {
+func (j *JwtWrapper) ValidateToken(c *gin.Context, signedToken string, isAdmin bool) (claims *JwtClaim, err error) {
 	token, err := jwt.ParseWithClaims(signedToken, &JwtClaim{}, func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
@@ -54,20 +56,30 @@ func (j *JwtWrapper) ValidateToken(signedToken string, isAdmin bool) (claims *Jw
 		return []byte(j.SecretKey), nil
 	}, jwt.WithLeeway(5*time.Second))
 	if err != nil {
+		c.Set("errorStatus", http.StatusUnauthorized)
+		c.Set("errorMessage", err.Error())
 		return nil, err
 	} else if claims, ok := token.Claims.(*JwtClaim); ok {
 		if claims.ExpiresAt.Time.Unix() < time.Now().Unix() {
+			c.Set("errorStatus", http.StatusUnauthorized)
+			c.Set("errorMessage", "the session has expired, token refreshing is needed")
 			return nil, errors.New("the session has expired, token refreshing is needed")
 		} else {
 			if isAdmin && !claims.IsAdmin {
+				c.Set("errorStatus", http.StatusBadRequest)
+				c.Set("errorMessage", "the email provided is not an administrator email")
 				return nil, errors.New("the email provided is not an administrator email")
 			}
 			if !isAdmin && claims.IsAdmin {
+				c.Set("errorStatus", http.StatusBadRequest)
+				c.Set("errorMessage", "the email provided is an administrator email, please use the /admin path instead")
 				return nil, errors.New("the email provided is an administrator email, please use the /admin path instead")
 			}
 			return claims, nil
 		}
 	} else {
+		c.Set("errorStatus", http.StatusInternalServerError)
+		c.Set("errorMessage", "unknown claim type, cannot proceed")
 		return nil, errors.New("unknown claim type, cannot proceed")
 	}
 }
