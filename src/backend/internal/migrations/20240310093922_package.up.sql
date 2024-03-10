7e57352f0cd3833cbeac2523501614f74c9a9437
Roongkun <roongkun.l@gmail.com>
2024-03-10 16:56:35 +0700

    written sql commands

---
+++ src/backend/internal/migrations/20240310093922_package.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE packages(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    photographer_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    price integer NOT NULL
+);
+
+CREATE TABLE bookings(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    customer_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    package_id uuid NOT NULL REFERENCES packages(id),
+    start_time timestamptz NOT NULL,
+    end_time timestamptz NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+    deleted_at timestamptz
+);
+
