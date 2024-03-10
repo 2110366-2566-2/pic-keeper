cfb169dcf05591e19c067bae8a1de83a3fcd4061
Roongkun <roongkun.l@gmail.com>
2024-03-10 21:44:17 +0700

    tested

--- src/backend/internal/controller/photographer/get_one_package.go
+++ src/backend/internal/controller/photographer/get_one_package.go
@@ -10,7 +10,6 @@ import (
 func (r *Resolver) GetOnePackage(c *gin.Context) {
 	paramId := c.Param("id")
 	packageId := uuid.MustParse(paramId)
-
 	pkg, err := r.PackageUsecase.PackageRepo.FindOneById(c, packageId)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
