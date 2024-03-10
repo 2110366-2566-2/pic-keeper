1c3caf4ac09791d2ab7e9b626dee4ac7bbf31f45
Roongkun <roongkun.l@gmail.com>
2024-03-10 17:45:41 +0700

    finished c operation, untested

--- src/backend/internal/controller/photographer/create_package.go
+++ src/backend/internal/controller/photographer/create_package.go
@@ -1,7 +1,57 @@
 package photographer
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/Roongkun/software-eng-ii/internal/model"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 func (r *Resolver) CreatePackage(c *gin.Context) {
+	photographer, exists := c.Get("photographer")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "error": "Failed to retrieve photographer from context"})
+		c.Abort()
+		return
+	}
 
+	photographerObj, ok := photographer.(model.Photographer)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "error": "Invalid object type in context"})
+		c.Abort()
+		return
+	}
+
+	packageInput := model.PackageInput{}
+	if err := c.BindJSON(&packageInput); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failed",
+			"error":   err.Error(),
+			"message": "unable to bind request body with json, please recheck",
+		})
+		c.Abort()
+		return
+	}
+
+	newPackage := model.Package{
+		Id:             uuid.New(),
+		PhotographerId: photographerObj.Id,
+		Name:           packageInput.Name,
+		Price:          packageInput.Price,
+	}
+
+	if err := r.PackageUsecase.PackageRepo.AddOne(c, &newPackage); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": "failed",
+			"error":  err.Error(),
+		})
+		c.Abort()
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status": "success",
+		"data":   newPackage,
+	})
 }
