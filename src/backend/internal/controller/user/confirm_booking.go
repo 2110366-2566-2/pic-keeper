2a6888447ae97ed7a014756a89d26530ae8d6895
Roongkun <roongkun.l@gmail.com>
2024-03-11 13:07:59 +0700

    confirm booking

--- src/backend/internal/controller/user/confirm_booking.go
+++ src/backend/internal/controller/user/confirm_booking.go
@@ -1,1 +1,56 @@
 package user
+
+import (
+	"net/http"
+
+	"github.com/Roongkun/software-eng-ii/internal/model"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func (r *Resolver) ConfirmBooking(c *gin.Context) {
+	user := c.MustGet("user")
+	userObj, ok := user.(model.User)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "message": "Invalid user type in context"})
+		c.Abort()
+		return
+	}
+
+	paramId := c.Param("id")
+	bookingId := uuid.MustParse(paramId)
+
+	booking, err := r.BookingUsecase.BookingRepo.FindOneById(c, bookingId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": "failed",
+			"error":  err.Error(),
+		})
+		c.Abort()
+		return
+	}
+
+	if booking.CustomerId != userObj.Id {
+		c.JSON(http.StatusForbidden, gin.H{
+			"status": "failed",
+			"error":  "this booking is not yours",
+		})
+		c.Abort()
+		return
+	}
+
+	booking.Status = model.BookingConfirmedStatus
+	if err := r.BookingUsecase.BookingRepo.UpdateOne(c, booking); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": "failed",
+			"error":  err.Error(),
+		})
+		c.Abort()
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "success",
+		"data":   booking,
+	})
+}
