8d777a614de51c782047b430b5af2dd4cd41c512
Roongkun <roongkun.l@gmail.com>
2024-03-11 12:26:20 +0700

    checkpoint #2

---
+++ src/backend/internal/controller/user/my_bookings.go
@@ -0,0 +1,33 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/Roongkun/software-eng-ii/internal/model"
+	"github.com/gin-gonic/gin"
+)
+
+func (r *Resolver) MyBookings(c *gin.Context) {
+	user := c.MustGet("user")
+	userObj, ok := user.(model.User)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "message": "Invalid user type in context"})
+		c.Abort()
+		return
+	}
+
+	bookings, err := r.BookingUsecase.FindByUserId(c, userObj.Id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": "failed",
+			"error":  err.Error(),
+		})
+		c.Abort()
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "success",
+		"data":   bookings,
+	})
+}
