e1973c0eaad42ccbc360823539f5366f7ac90213
Maekin <74455990+mekintown@users.noreply.github.com>
2024-03-19 17:44:02 +0700

    Fix merge error

--- src/frontend/src/app/user/(non-navbar)/my-galleries/create-gallery/page.tsx
+++
@@ -1,9 +0,0 @@
-import CreateGallery from "@/components/CreateGallery";
-
-const Home = () => {
-  return (
-    <div className="max-w-4xl m-auto mx-4"><CreateGallery/></div>
-  );
-};
-
-export default Home;
