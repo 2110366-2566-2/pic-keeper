e224c6459af71cf4713b125fbb1a4fad901b984c
Maekin <74455990+mekintown@users.noreply.github.com>
2024-03-19 19:05:38 +0700

    Fix left side ui of create gallery to make it more responsive

--- src/frontend/src/components/CreateGallery.tsx
+++ src/frontend/src/components/CreateGallery.tsx
@@ -26,18 +26,18 @@ const CreateGallery = () => {
   };
 
   return (
-    <div className="mx-auto  rounded-lg shadow-lg  bg-white flex items-center justify-between p-4">
-      <div className="flex-1 flex flex-col">
-        <div className="flex items-center ml-12 mt-4">
-          <button className="text-black mr-5 text-6xl">
+    <div className="mx-auto rounded-lg shadow-lg flex items-center justify-between p-4">
+      <div className="flex-1 flex flex-col gap-5">
+        <div className="flex items-center justify-between">
+          <button className="text-black text-6xl">
             <IoIosArrowBack />
           </button>
 
-          <div className="flex flex-col items-center justify-center rounded-lg w-[25vw] h-[30vh]">
+          <div className="flex flex-col items-center justify-center rounded-lg">
             {!file && (
-              <div className="flex flex-col items-center justify-center rounded-lg w-full h-full bg-[#FBFAFA] p-4">
+              <div className="flex flex-col items-center justify-center rounded-lg bg-gray-50 px-24 py-4">
                 <Image
-                  src="/images/UploadToCloud.png"
+                  src="/images/upload-to-cloud.png"
                   alt="Upload"
                   width={100}
                   height={100}
@@ -49,7 +49,7 @@ const CreateGallery = () => {
                   onChange={handleFileChange}
                 />
                 <button
-                  className="mt-8 text-white bg-[#E19007] rounded px-6 py-1"
+                  className="mt-8 text-white bg-amber-500 rounded px-6 py-1"
                   onClick={handleBrowseClick}
                 >
                   Browse
@@ -72,14 +72,14 @@ const CreateGallery = () => {
             )}
           </div>
 
-          <button className="text-black ml-5 text-6xl">
+          <button className="text-black text-6xl">
             <IoIosArrowForward />
           </button>
         </div>
 
-        <div className="mt-20 ml-10">
+        <div>
           <div className="group">
-            <div className="flex items-center justify-center rounded-2xl bg-[#FBFAFA] relative overflow-hidden w-[7vw] h-[18vh] border-4 border-transparent group-hover:border-yellow-500 group-hover:shadow-outline">
+            <div className="flex items-center justify-center rounded-2xl bg-gray-50 overflow-hidden max-w-[7vw] py-5 border-4 border-transparent group-hover:border-yellow-500 group-hover:shadow-outline">
               {!file && <HiOutlinePlusSm className="text-gray-500 text-8xl" />}
               {file && (
                 <div className="rounded-2xl overflow-hidden w-full h-full relative">
@@ -97,86 +97,76 @@ const CreateGallery = () => {
       </div>
 
       <div className="flex-1">
-        <div className="flex flex-col overflow-y-auto gap-2">
-          <div className="flex flex-col">
-            <label htmlFor="gallery" className="label-normal">
-              Gallery name
-            </label>
-            <input id="gallery" type="text" className="form-input" />
-          </div>
-
-          <div className="flex flex-col">
-            <label htmlFor="description" className="label-normal">
-              Description
-            </label>
-            <textarea id="description" rows={4} className="form-input" />
-          </div>
-
-          <div className="flex items-center gap-4">
-            <div>
-              <label htmlFor="hours" className="label-normal">
-                Hours
+        <div className="overflow-y-scroll h-[62vh]">
+          <div className="flex flex-col overflow-y-auto gap-2">
+            <div className="flex flex-col">
+              <label htmlFor="gallery" className="label-normal">
+                Gallery name
               </label>
-              <div className="flex items-center gap-2">
-                <input
-                  id="hours"
-                  type="text"
-                  className="form-input px-0"
-                ></input>
-                <span className="label-normal">Hrs</span>
-              </div>
+              <input id="gallery" type="text" className="form-input" />
             </div>
-
-            <div className="flex-grow">
-              <label htmlFor="location" className="label-normal">
-                Location
+            <div className="flex flex-col">
+              <label htmlFor="description" className="label-normal">
+                Description
               </label>
-              <div className="relative">
-                <input id="location" type="text" className="form-input" />
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                  <GrLocation />
+              <textarea id="description" rows={4} className="form-input" />
+            </div>
+            <div className="flex items-center gap-4">
+              <div>
+                <label htmlFor="hours" className="label-normal">
+                  Hours
+                </label>
+                <div className="flex items-center gap-2">
+                  <input id="hours" type="text" className="form-input"></input>
+                  <span className="label-normal">Hrs</span>
+                </div>
+              </div>
+              <div className="flex-grow">
+                <label htmlFor="location" className="label-normal">
+                  Location
+                </label>
+                <div className="relative">
+                  <input id="location" type="text" className="form-input" />
+                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                    <GrLocation />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <label htmlFor="deliveryTime" className="label-normal">
+                  Delivery time
+                </label>
+                <div className="flex items-center gap-2">
+                  <input id="deliveryTime" type="text" className="form-input" />
+                  <span className="label-normal">Days</span>
                 </div>
               </div>
             </div>
-
-            <div>
-              <label htmlFor="deliveryTime" className="label-normal">
-                Delivery time
-              </label>
-              <div className="flex items-center gap-2">
-                <input
-                  id="deliveryTime"
-                  type="text"
-                  className="form-input px-0"
-                />
-                <span className="label-normal">Days</span>
-              </div>
+            <div className="flex flex-col">
+              <span className="mr-2">What includes in this package</span>
             </div>
-          </div>
-          <div className="flex flex-col">
-            <span className="mr-2">What includes in this package</span>
-          </div>
-          {[...Array(additionalInputs)].map((_, index) => (
-            <input key={index} type="text" className="form-input" />
-          ))}
-          {/* Click to add button */}
-          <div className="flex items-center">
-            <button
-              className="text-yellow-600 flex items-center"
-              onClick={handleAddInput}
-            >
-              <IoIosAdd className="w-8 h-auto" />
-              Click to add
-            </button>
-          </div>
-          <div className="flex items-center">
-            <div className="flex flex-col ">
-              <label id="price" className="label-normal">
-                Price
-              </label>
-              <div className="flex items-center gap-2">
-                <input type="text" className="form-input" />
-                <span className="label-normal">THB</span>
+            {[...Array(additionalInputs)].map((_, index) => (
+              <input key={index} type="text" className="form-input" />
+            ))}
+            {/* Click to add button */}
+            <div className="flex items-center">
+              <button
+                className="text-yellow-600 flex items-center"
+                onClick={handleAddInput}
+              >
+                <IoIosAdd className="w-8 h-auto" />
+                Click to add
+              </button>
+            </div>
+            <div className="flex items-center">
+              <div className="flex flex-col ">
+                <label id="price" className="label-normal">
+                  Price
+                </label>
+                <div className="flex items-center gap-2">
+                  <input type="text" className="form-input" />
+                  <span className="label-normal">THB</span>
+                </div>
               </div>
             </div>
           </div>
