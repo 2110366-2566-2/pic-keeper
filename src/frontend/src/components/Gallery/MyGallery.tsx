909d9e7d4b13aa605a6232240c39319ed7236ec4
Maekin <74455990+mekintown@users.noreply.github.com>
2024-03-20 20:48:56 +0700

    Improve typography

--- src/frontend/src/components/Gallery/MyGallery.tsx
+++ src/frontend/src/components/Gallery/MyGallery.tsx
@@ -7,6 +7,8 @@ import ImageViewer from "./ImageViewer";
 import ProfileImage from "../shared/ProfileImage";
 import { User } from "@/types/user";
 import userService from "@/services/user";
+import { IoIosCamera, IoIosLocate, IoIosTime } from "react-icons/io";
+import { IoLocateSharp, IoLocation, IoLocationSharp } from "react-icons/io5";
 
 interface Props {
   galleryId: string;
@@ -47,41 +49,54 @@ const MyGallery = ({ galleryId }: Props) => {
     return <div>No gallery or photographer specified</div>;
   }
   return (
-    <div className="mx-auto rounded-lg shadow-lg p-4 grid grid-cols-4 gap-8">
+    <div className="mx-auto rounded-lg shadow-lg p-6 grid grid-cols-1 md:grid-cols-4 gap-8 bg-white text-gray-800">
       <ImageViewer imageUrls={imageUrls} />
 
-      <div className="col-span-2 flex-1 space-y-4">
-        <h1 className="text-title">{gallery.name}</h1>
-        <div className="flex self-stretch gap-4">
-          <ProfileImage src={profilePicture} size={14} />
+      <div className="md:col-span-2 space-y-6">
+        <h1 className="text-3xl font-bold text-gray-900 leading-tight">
+          {gallery.name}
+        </h1>
+        <div className="flex items-center gap-4">
+          <ProfileImage src={profilePicture} size={16} />
           <div>
-            <h2 className="text-md tracking-wide">
+            <h2 className="text-xl font-semibold text-gray-800">
               {photographer.firstname} {photographer.lastname}
             </h2>
-            <h3 className="text-standard">{photographer.gender}</h3>
+            <h3 className="text-md text-gray-600">{photographer.gender}</h3>
           </div>
         </div>
         <div>
-          <h2 className="text-md tracking-wide">Description</h2>
-          <div className="space-y-2">
-            <p className="text-base text-gray-900">{gallery.description}</p>
-          </div>
+          <h2 className="text-xl font-semibold text-gray-900">Description</h2>
+          <p className="text-base text-gray-700 leading-relaxed">
+            {gallery.description}
+          </p>
         </div>
-        <div className="space-y-4">
-          <div className="flex justify-between">
-            <h2>Package</h2>
-            <h2>${gallery.price}</h2>
+        <div className="space-y-4 rounded-xl ring-1 p-4 ring-gray-300">
+          <div className="flex justify-between items-center">
+            <h2 className="text-lg font-semibold text-gray-900">Package</h2>
+            <p className="text-lg font-bold text-amber-600">
+              {gallery.price} THB
+            </p>
           </div>
-          <div className="space-y-2">
-            <h4>{gallery.hours} hours photography event</h4>
-            <h4>{gallery.location}</h4>
-            <h4>{gallery.delivery_time} days delivery</h4>
-          </div>
-          <div>
-            {gallery.included.map((include) => (
-              <li key={include}>{include}</li>
+          <ul className="space-y-2 text-gray-700">
+            <li className="flex gap-2 items-center">
+              <IoIosCamera className="w-6 h-6" />
+              <span>{gallery.hours} hours photography event</span>
+            </li>
+            <li className="flex gap-2 items-center">
+              <IoLocationSharp className="w-6 h-6" />
+              <span>{gallery.location}</span>
+            </li>
+            <li className="flex gap-2 items-center">
+              <IoIosTime className="w-6 h-6" />
+              <span>{gallery.delivery_time} days delivery</span>
+            </li>
+            {gallery.included.map((include, index) => (
+              <li key={index} className="list-disc list-inside text-gray-400">
+                <span className="text-gray-600">{include}</span>
+              </li>
             ))}
-          </div>
+          </ul>
         </div>
       </div>
     </div>
