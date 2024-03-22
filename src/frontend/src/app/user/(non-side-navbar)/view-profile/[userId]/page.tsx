4cfb6689c85c84ae4ab18531d805d67d8a4a0595
WorkingDada <6430222521@student.chula.ac.th>
2024-03-22 23:01:14 +0700

    Added About Location Contact view

--- src/frontend/src/app/user/(non-side-navbar)/view-profile/[userId]/page.tsx
+++ src/frontend/src/app/user/(non-side-navbar)/view-profile/[userId]/page.tsx
@@ -9,22 +9,14 @@ import Image from "next/image";
 import { capitalizeFirstLetter } from "@/utils/string";
 
 const Home = ({ params }: { params: { userId: string } }) => {
-  // const [profileUser, setProfileUser] = useState<User>()
   const { data: session } = useSession();
   const [photoPreview, setPhotoPreview] = useState<string | null>(null);
-  // useEffect(() => {
-  //   const fetchUser = async () => {
-  //     const response = await userService.getUserById(params.userId)
-  //     if (response.data)  setProfileUser(response.data)
-  //   }
-  //   fetchUser();
-  // }, [])
 
   return (
-    <main className="m-8 space-y-4">
-      <div className="text-2xl">Profile</div>
+    <main className="m-8 space-y-6">
+      <div className="text-2xl font-semibold px-5">Profile</div>
       <div className="w-full rounded-md h-28 shadow-md space-x-2 relative flex flex-row">
-        <div className="bg-gray-300 rounded-full w-24 h-24 justify-items-center">
+        <div className="bg-gray-300 rounded-full w-24 h-24 justify-items-center ml-10 mr-5">
           {photoPreview && (
             <Image
               src={photoPreview}
@@ -49,7 +41,11 @@ const Home = ({ params }: { params: { userId: string } }) => {
             <div className="text-xl">{session?.user.data?.firstname}</div>
             <div className="text-xl">{session?.user.data?.lastname}</div>
           </div>
-          <div className="text-sm text-gray-500">{session?.user.data && capitalizeFirstLetter(session.user.data.gender) || "Not specified"}</div>
+          <div className="text-sm text-gray-500">
+            {(session?.user.data &&
+              capitalizeFirstLetter(session.user.data.gender)) ||
+              "Not specified"}
+          </div>
         </div>
         <Link
           href="/user/edit-profile"
@@ -57,6 +53,28 @@ const Home = ({ params }: { params: { userId: string } }) => {
         >
           Edit
         </Link>
+      </div>
+      <div className="flex flex-row space-x-6">
+        <div className="w-3/12 shadow-md rounded-md p-4 space-y-4">
+          <div className="text-xl font-semibold text-amber-500">About</div>
+          <article className="text-wrap text-md text-gray-600">
+            <p>{session?.user.data?.about || "No description provided"}</p>
+          </article>
+          <div className="text-xl font-semibold text-amber-500">Location</div>
+          <article className="text-wrap text-md text-gray-600">
+            <p>{session?.user.data?.address || "No description provided"}</p>
+          </article>
+          <div className="text-xl font-semibold text-amber-500">Contact</div>
+          <article className="text-wrap text-md text-gray-600">
+            <p>{session?.user.data?.email || ""}</p>
+            <p>{session?.user.data?.phone_number || "No description provided"}</p>
+          </article>
+        </div>
+        <div className="w-9/12 shadow-md rounded-md">
+          <div className="text-amber-500 font-semibold text-xl p-4">
+            Galleries
+          </div>
+        </div>
       </div>
     </main>
   );
