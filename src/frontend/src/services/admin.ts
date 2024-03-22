e7db06bd455463739f83243fb2b79f6cad16babd
Maekin <74455990+mekintown@users.noreply.github.com>
2024-03-22 09:48:07 +0700

    Update admin service and fix backend typo

--- src/frontend/src/services/admin.ts
+++ src/frontend/src/services/admin.ts
@@ -12,38 +12,10 @@ import { signOut } from "next-auth/react";
 
 const adminBaseUrl = "/admin/v1";
 
-const login = async (loginCredentials: LoginCredentials) => {
-  try {
-    const { data } = await apiClient.post<LoginResponse>(
-      `${adminBaseUrl}/login`,
-      loginCredentials
-    );
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const refresh = async (token: string) => {
-  try {
-    const { data } = await apiClient.get<RefreshTokenResponse>(
-      `${adminBaseUrl}/refresh`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
-
 const listPendingPhotographer = async () => {
   try {
     const { data } = await apiClientWithAuth.get<UserListResponse>(
-      `${adminBaseUrl}/verifications/pending-photographers`
+      `${adminBaseUrl}/pending-photographers`
     );
     return data;
   } catch (error) {
@@ -54,7 +26,7 @@ const listPendingPhotographer = async () => {
 const verify = async (id: string) => {
   try {
     const { data } = await apiClientWithAuth.put<UserResponse>(
-      `${adminBaseUrl}/verifications/verify/${id}`
+      `${adminBaseUrl}/verify/${id}`
     );
     return data;
   } catch (error) {
@@ -65,20 +37,8 @@ const verify = async (id: string) => {
 const reject = async (id: string) => {
   try {
     const { data } = await apiClientWithAuth.put<UserResponse>(
-      `${adminBaseUrl}/verifications/reject/${id}`
+      `${adminBaseUrl}/reject/${id}`
     );
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const logout = async () => {
-  try {
-    const { data } = await apiClientWithAuth.put<LogoutResponse>(
-      `${adminBaseUrl}/logout`
-    );
-    signOut();
     return data;
   } catch (error) {
     throw error;
@@ -86,12 +46,9 @@ const logout = async () => {
 };
 
 const adminService = {
-  login,
-  refresh,
   listPendingPhotographer,
   verify,
   reject,
-  logout,
 };
 
 export default adminService;
