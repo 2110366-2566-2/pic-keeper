56515e0efb99c017b650279ca3fad26081d7f1a8
WorkingDada <6430222521@student.chula.ac.th>
2024-03-22 23:08:56 +0700

    Added working profile picture

--- src/frontend/src/app/user/(non-side-navbar)/view-profile/[userId]/page.tsx
+++ src/frontend/src/app/user/(non-side-navbar)/view-profile/[userId]/page.tsx
@@ -10,25 +10,40 @@ import { capitalizeFirstLetter } from "@/utils/string";
 
 const Home = ({ params }: { params: { userId: string } }) => {
   const { data: session } = useSession();
-  const [photoPreview, setPhotoPreview] = useState<string | null>(null);
+  const [profilePicture, setProfilePicture] = useState("");
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await userService.getMyUserInfo();
+        if (response.data) {
+          setProfilePicture(response.profile_picture_url);
+        }
+      } catch (error) {
+        console.log("error");
+      }
+    };
+
+    fetchUserInfo();
+  }, [session]);
 
   return (
     <main className="m-8 space-y-6">
       <div className="text-2xl font-semibold px-5">Profile</div>
       <div className="w-full rounded-md h-28 shadow-md space-x-2 relative flex flex-row">
-        <div className="bg-gray-300 rounded-full w-24 h-24 justify-items-center ml-10 mr-5">
-          {photoPreview && (
-            <Image
-              src={photoPreview}
-              alt="Profile Preview"
-              fill={true}
-              className="rounded-full object-cover" // Make the image circular
-            />
-          )}
+        <div className="relative bg-gray-300 rounded-full w-24 h-24 justify-items-center ml-10 mr-5">
+          <Image
+            className=" object-cover rounded-full"
+            fill={true}
+            src={session ? profilePicture : "/images/no-picture.jpeg"}
+            alt=""
+          />
         </div>
-        <div className="flex flex-col col-span-4">
+        <div className="flex flex-col col-span-4 space-y-1">
           <div className="flex flex-row space-x-3">
-            <div className="text-xl">{session?.user.data?.username}</div>
+            <div className="text-xl font-semibold">
+              {session?.user.data?.username}
+            </div>
             {session?.user.data?.verification_status === "VERIFIED" ? (
               <div className="p-1 bg-neutral-300 rounded-2xl text-neutral-500 text-xs">
                 Photograher
@@ -38,8 +53,12 @@ const Home = ({ params }: { params: { userId: string } }) => {
             )}
           </div>
           <div className="flex flex-row space-x-2">
-            <div className="text-xl">{session?.user.data?.firstname}</div>
-            <div className="text-xl">{session?.user.data?.lastname}</div>
+            <div className="text-xl font-semibold">
+              {session?.user.data?.firstname}
+            </div>
+            <div className="text-xl font-semibold">
+              {session?.user.data?.lastname}
+            </div>
           </div>
           <div className="text-sm text-gray-500">
             {(session?.user.data &&
@@ -67,7 +86,9 @@ const Home = ({ params }: { params: { userId: string } }) => {
           <div className="text-xl font-semibold text-amber-500">Contact</div>
           <article className="text-wrap text-md text-gray-600">
             <p>{session?.user.data?.email || ""}</p>
-            <p>{session?.user.data?.phone_number || "No description provided"}</p>
+            <p>
+              {session?.user.data?.phone_number || "No description provided"}
+            </p>
           </article>
         </div>
         <div className="w-9/12 shadow-md rounded-md">
