5aafdccd9a26ec3af3c57676589a616053ee4948
Roongkun <roongkun.l@gmail.com>
2024-03-23 23:14:15 +0700

    finished sql queries

--- src/backend/internal/repository/postgres/issue.go
+++ src/backend/internal/repository/postgres/issue.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"time"
 
 	"github.com/Roongkun/software-eng-ii/internal/model"
 	"github.com/uptrace/bun"
@@ -46,5 +47,34 @@ func (i *IssueDB) FindIssuesWithFilter(ctx context.Context, filters model.IssueF
 	return issues, nil
 }
 
-func (i *IssueDB) FindHeaderMetadata(ctx context.Context) ([]*model.IssueHeaderMetadata, error) {
+func (i *IssueDB) FindHeaderMetadata(ctx context.Context) (*model.IssueHeaderMetadata, error) {
+	var issue model.Issue
+	var result model.IssueHeaderMetadata
+
+	count, err := i.db.NewSelect().Model(&issue).Where("status = ?", model.IssueOpenStatus).Count(ctx)
+	if err != nil {
+		return nil, err
+	}
+	result.PendingTickets = count
+
+	date := time.Now().Format("2024-01-01")
+	count, err = i.db.NewSelect().Model(&issue).Where("DATE(created_at) = ?", date).Count(ctx)
+	if err != nil {
+		return nil, err
+	}
+	result.TicketsToday = count
+
+	count, err = i.db.NewSelect().Model(&issue).Where("DATE(due_date) = ?", date).Count(ctx)
+	if err != nil {
+		return nil, err
+	}
+	result.TicketsDueToday = count
+
+	count, err = i.db.NewSelect().Model(&issue).Where("status = ?", model.IssueClosedStatus).Count(ctx)
+	if err != nil {
+		return nil, err
+	}
+	result.ClosedTickets = count
+
+	return &result, nil
 }
