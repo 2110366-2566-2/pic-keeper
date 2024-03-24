7b24e998cc7416ba235611fb9d93a5959e8dec2d
Maekin <74455990+mekintown@users.noreply.github.com>
2024-03-24 15:08:17 +0700

    Fix admin service merge conflict

--- src/frontend/src/services/admin.ts
+++ src/frontend/src/services/admin.ts
@@ -1,108 +1,12 @@
-<<<<<<< HEAD
-import apiClient from "@/libs/apiClient";
-import { LoginCredentials } from "@/types/auth";
-import {
-  LoginResponse,
-  LogoutResponse,
-  RefreshTokenResponse,
-  UserListResponse,
-  UserResponse,
-} from "@/types/response";
-import { Axios } from "axios";
-import { signOut } from "next-auth/react";
-||||||| 4be7c59
-import apiClient from "@/libs/apiClient";
-import apiClientWithAuth from "@/libs/apiClientWithAuth";
-import { LoginCredentials } from "@/types/auth";
-import {
-  LoginResponse,
-  LogoutResponse,
-  RefreshTokenResponse,
-  UserListResponse,
-  UserResponse,
-} from "@/types/response";
-import { signOut } from "next-auth/react";
-=======
-import apiClientWithAuth from "@/libs/apiClientWithAuth";
 import { UserListResponse, UserResponse } from "@/types/response";
->>>>>>> 1d9df7e442fbdd260f40258842ff7e304d8c39a9
+import apiClientWithAuth from "@/libs/apiClientWithAuth";
 
 const adminBaseUrl = "/admin/v1";
 
-<<<<<<< HEAD
-const login = async (loginCredentials: LoginCredentials) => {
-  try {
-    const { data } = await apiClient.post<LoginResponse>(
-      `${adminBaseUrl}/login`,
-      loginCredentials
-    );
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const refreshToken = async (token: string) => {
-  try {
-    const { data } = await apiClient.get<RefreshTokenResponse>(
-      `${adminBaseUrl}/refresh`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const listPendingPhotographer = async (apiWithAuthForAdmin: Axios) => {
-||||||| 4be7c59
-const login = async (loginCredentials: LoginCredentials) => {
-  try {
-    const { data } = await apiClient.post<LoginResponse>(
-      `${adminBaseUrl}/login`,
-      loginCredentials
-    );
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const refresh = async (token: string) => {
-  try {
-    const { data } = await apiClient.get<RefreshTokenResponse>(
-      `${adminBaseUrl}/refresh`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
-
 const listPendingPhotographer = async () => {
-=======
-const listPendingPhotographer = async () => {
->>>>>>> 1d9df7e442fbdd260f40258842ff7e304d8c39a9
   try {
-<<<<<<< HEAD
-    const { data } = await apiWithAuthForAdmin.get<UserListResponse>(
-      `${adminBaseUrl}/verifications/pending-photographers`
-||||||| 4be7c59
-    const { data } = await apiClientWithAuth.get<UserListResponse>(
-      `${adminBaseUrl}/verifications/pending-photographers`
-=======
     const { data } = await apiClientWithAuth.get<UserListResponse>(
       `${adminBaseUrl}/pending-photographers`
->>>>>>> 1d9df7e442fbdd260f40258842ff7e304d8c39a9
     );
     return data;
   } catch (error) {
@@ -110,18 +14,10 @@ const listPendingPhotographer = async () => {
   }
 };
 
-const verify = async (apiWithAuthForAdmin: Axios, id: string) => {
+const verify = async (id: string) => {
   try {
-<<<<<<< HEAD
-    const { data } = await apiWithAuthForAdmin.put<UserResponse>(
-      `${adminBaseUrl}/verifications/verify/${id}`
-||||||| 4be7c59
-    const { data } = await apiClientWithAuth.put<UserResponse>(
-      `${adminBaseUrl}/verifications/verify/${id}`
-=======
     const { data } = await apiClientWithAuth.put<UserResponse>(
       `${adminBaseUrl}/verify/${id}`
->>>>>>> 1d9df7e442fbdd260f40258842ff7e304d8c39a9
     );
     return data;
   } catch (error) {
@@ -129,18 +25,10 @@ const verify = async (apiWithAuthForAdmin: Axios, id: string) => {
   }
 };
 
-const reject = async (apiWithAuthForAdmin: Axios, id: string) => {
+const reject = async (id: string) => {
   try {
-<<<<<<< HEAD
-    const { data } = await apiWithAuthForAdmin.put<UserResponse>(
-      `${adminBaseUrl}/verifications/reject/${id}`
-||||||| 4be7c59
-    const { data } = await apiClientWithAuth.put<UserResponse>(
-      `${adminBaseUrl}/verifications/reject/${id}`
-=======
     const { data } = await apiClientWithAuth.put<UserResponse>(
       `${adminBaseUrl}/reject/${id}`
->>>>>>> 1d9df7e442fbdd260f40258842ff7e304d8c39a9
     );
     return data;
   } catch (error) {
@@ -148,43 +36,7 @@ const reject = async (apiWithAuthForAdmin: Axios, id: string) => {
   }
 };
 
-<<<<<<< HEAD
-const logout = async (apiWithAuthForAdmin: Axios) => {
-  try {
-    const { data } = await apiWithAuthForAdmin.put<LogoutResponse>(
-      `${adminBaseUrl}/logout`
-    );
-    signOut();
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-||||||| 4be7c59
-const logout = async () => {
-  try {
-    const { data } = await apiClientWithAuth.put<LogoutResponse>(
-      `${adminBaseUrl}/logout`
-    );
-    signOut();
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-=======
->>>>>>> 1d9df7e442fbdd260f40258842ff7e304d8c39a9
 const adminService = {
-<<<<<<< HEAD
-  login,
-  refreshToken,
-||||||| 4be7c59
-  login,
-  refresh,
-=======
->>>>>>> 1d9df7e442fbdd260f40258842ff7e304d8c39a9
   listPendingPhotographer,
   verify,
   reject,
