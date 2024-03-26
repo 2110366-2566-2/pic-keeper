d6d2a633d62ff71fd744b487343717303a1e3ff1
haiibarose <ruk.rossukhon@gmail.com>
2024-03-26 20:37:36 +0700

    add cancel button action

--- src/frontend/src/components/Popup/ReportIssue.tsx
+++ src/frontend/src/components/Popup/ReportIssue.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useModal } from "@/context/ModalContext";
+import userService from '@/services/user';
+import { useErrorModal } from '@/hooks/useErrorModal';
 
 interface ReportIssueProps {
     onCancel?: () => void;
@@ -7,24 +9,37 @@ interface ReportIssueProps {
 
 const ReportIssue: React.FC<ReportIssueProps> = ({ onCancel }) => {
     const [issue, setIssue] = useState<string>('');
-    const { openModal } = useModal();
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        alert(`Issue reported: ${issue}`);
-        setIssue('');
-
-        openModal(
-            <div className="p-4 max-w-md mx-auto bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Issue Reported Successfully</h5>
-                <p className="text-base text-gray-500 dark:text-gray-400">Thank you for reporting the issue. We will look into it as soon as possible.</p>
-                <div className="flex justify-end mt-4">
-                    <button className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500" onClick={() => {/* Implement closing logic if necessary */ }}>Close</button>
-                </div>
-            </div>,
-            "Your report is submitted"
-        );
+    const { openModal, closeModal } = useModal();
+    const showError = useErrorModal();
+    const handleCancel = () => {
+        if (onCancel) {
+            onCancel();
+        } else {
+            closeModal();
+        }
     };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try {
+            const response = await userService.reportIssue({ description: issue });
+            setIssue('');
+
+            openModal(
+                <div className="p-4 max-w-md mx-auto bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Issue Reported Successfully</h5>
+                    <p className="text-base text-gray-500 dark:text-gray-400">Thank you for reporting the issue. We will look into it as soon as possible.</p>
+                    <div className="flex justify-end mt-4">
+                        <button className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500" onClick={() => { closeModal() }}>Close</button>
+                    </div>
+                </div>,
+                "Your report is submitted"
+            );
+        } catch (error) {
+            showError(error);
+        }
+    };
+
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-40">
@@ -42,7 +57,7 @@ const ReportIssue: React.FC<ReportIssueProps> = ({ onCancel }) => {
                         placeholder="Describe your issue here"
                     />
                     <div className="flex justify-center mt-5">
-                        <button type="button" onClick={onCancel} className="bg-white text-gray-600 py-2 px-5 border-none rounded-md cursor-pointer text-lg mr-15">
+                        <button type="button" onClick={handleCancel} className="bg-white text-gray-600 py-2 px-5 border-none rounded-md cursor-pointer text-lg mr-15">
                             Cancel
                         </button>
                         <button type="submit" className="bg-yellow-500 text-white py-2 px-5 border-none rounded-md cursor-pointer text-lg">
--- src/frontend/src/components/shared/Navbar/ProfileMenu.tsx
+++ src/frontend/src/components/shared/Navbar/ProfileMenu.tsx
@@ -5,95 +5,117 @@ import { Menu, Transition } from "@headlessui/react";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { Fragment, ReactNode } from "react";
+import { Fragment, ReactNode, useState } from "react";
 import { MdArrowDropUp, MdOutlineArrowDropDown } from "react-icons/md";
 import ProfileImage from "../ProfileImage";
+import ReportIssue from "@/components/Popup/ReportIssue";
 
-interface Props {
-  href: string;
-  children: ReactNode;
+interface ReportIssueProps {
+  onClose?: () => void;
 }
 
-const MenuItem = ({ href, children }: Props) => (
+interface Props {
+  href?: string;
+  onClick?: () => void;
+  children: ReactNode;
+}
+const MenuItem = ({ href, onClick, children }: Props) => (
   <Menu.Item>
     {({ active }) => (
-      <Link
-        href={href}
-        className={classNames(
-          active ? "bg-gray-100" : "",
-          "block px-4 py-2 text-sm z-50"
-        )}
-      >
-        {children}
-      </Link>
+      href ? (
+        <Link
+          href={href}
+          className={classNames(
+            active ? "bg-gray-100" : "",
+            "block px-4 py-2 text-sm z-50"
+          )}
+        >
+          {children}
+        </Link>
+      ) : (
+        <button
+          onClick={onClick}
+          className={classNames(
+            active ? "bg-gray-100" : "",
+            "block px-4 py-2 text-sm w-full text-left z-50"
+          )}
+        >
+          {children}
+        </button>
+      )
     )}
   </Menu.Item>
 );
 
 const ProfileMenu = () => {
   const { data: session } = useSession();
-  return (
-    <Menu as="div" className="relative ml-3">
-      {({ open }) => (
-        <>
-          <Menu.Button className="flex items-center gap-2 text-sm rounded-full focus:ring-2 focus:ring-offset-2 focus:outline-none">
-            <ProfileImage
-              src={session?.user?.profile_picture_url || "/images/nature.svg"}
-              size={8}
-            />
+  const [isReportIssueOpen, setReportIssueOpen] = useState(false);
 
-            <span>{session?.user.data?.firstname || "Guest"}</span>
-            {open ? <MdArrowDropUp /> : <MdOutlineArrowDropDown />}
-          </Menu.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute right-0 z-50 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              {!session ? (
-                <Menu.Item>
-                  {({ active }) => (
-                    <Link
-                      href="/auth/login"
-                      className={classNames(
-                        active ? "bg-gray-100" : "",
-                        "block px-4 py-2 text-sm"
-                      )}
-                    >
-                      Login / Register
-                    </Link>
-                  )}
-                </Menu.Item>
-              ) : (
-                <>
-                  <MenuItem href={`/view-profile/${session.user.data?.id}`}>Your Profile</MenuItem>
-                  <MenuItem href="/settings/edit-profile">Settings</MenuItem>
-                  <MenuItem href="/report-issues">Report issues</MenuItem>
+  return (
+    <>
+      <Menu as="div" className="relative ml-3">
+        {({ open }) => (
+          <>
+            <Menu.Button className="flex items-center gap-2 text-sm rounded-full focus:ring-2 focus:ring-offset-2 focus:outline-none">
+              <ProfileImage
+                src={session?.user?.profile_picture_url || "/images/nature.svg"}
+                size={8}
+              />
+
+              <span>{session?.user.data?.firstname || "Guest"}</span>
+              {open ? <MdArrowDropUp /> : <MdOutlineArrowDropDown />}
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-50 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                {!session ? (
                   <Menu.Item>
                     {({ active }) => (
-                      <button
-                        onClick={() => signOut()}
+                      <Link
+                        href="/auth/login"
                         className={classNames(
                           active ? "bg-gray-100" : "",
                           "block px-4 py-2 text-sm"
                         )}
                       >
-                        Logout
-                      </button>
+                        Login / Register
+                      </Link>
                     )}
                   </Menu.Item>
-                </>
-              )}
-            </Menu.Items>
-          </Transition>
-        </>
-      )}
-    </Menu>
+                ) : (
+                  <>
+                    <MenuItem href={`/view-profile/${session.user.data?.id}`}>Your Profile</MenuItem>
+                    <MenuItem href="/settings/edit-profile">Settings</MenuItem>
+                    <MenuItem onClick={() => setReportIssueOpen(true)}>Report issues</MenuItem>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          onClick={() => signOut()}
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm"
+                          )}
+                        >
+                          Logout
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </>
+                )}
+              </Menu.Items>
+            </Transition>
+          </>
+        )}
+      </Menu>
+      {isReportIssueOpen && <ReportIssue onCancel={() => setReportIssueOpen(false)} />}
+    </>
   );
 };
 
