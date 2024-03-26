6bf8688bf399e12bdef4cdd01ffed8a4755efb2d
Maekin <74455990+mekintown@users.noreply.github.com>
2024-03-27 01:19:40 +0700

    Add draft for connecting with backend

--- src/frontend/src/components/Chat/BookingBtn.tsx
+++ src/frontend/src/components/Chat/BookingBtn.tsx
@@ -1,3 +1,6 @@
+import { useModal } from "@/context/ModalContext";
+import { useErrorModal } from "@/hooks/useErrorModal";
+import { photographerBookingService } from "@/services";
 import { Booking, BookingStatus } from "@/types/booking";
 import { Room } from "@/types/room";
 import { useSession } from "next-auth/react";
@@ -9,9 +12,32 @@ interface Props {
 
 const BookingBtn = ({ room, booking }: Props) => {
   const { data: session } = useSession();
+  const { openModal, closeModal } = useModal();
+  const showError = useErrorModal();
 
-  const handlePhotographerEditBooking = () => {};
+  const handlePhotographerSaveBooking = async () => {
+    try {
+      // TODO: Implement this kub
+      // const response = await photographerBookingService.createBooking({Your booking proposal});
+      closeModal();
+    } catch (error) {
+      showError(error);
+    }
+  };
+  const handlePhotographerEditBooking = () => {
+    openModal(
+      // TODO: Here
+      <form onSubmit={handlePhotographerSaveBooking}>
+        Please implement this kub
+        <button className="btn-primary px-6 py-2" type="submit">
+          Save
+        </button>
+      </form>,
+      "Edit package"
+    );
+  };
 
+  // TODO: Here
   const handleCustomerBooking = () => {};
 
   if (room.gallery.photographer_id === session?.user.data?.id) {
