c25eb459d7847b4519eeabd43a33b7b4c5952af6
haiibarose <ruk.rossukhon@gmail.com>
2024-03-27 20:35:10 +0700

    finish payment successful popup

--- src/frontend/src/components/Popup/PaymentSuccess.tsx
+++ src/frontend/src/components/Popup/PaymentSuccess.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useModal } from "@/context/ModalContext";
+import userService from '@/services/user';
+import { useErrorModal } from '@/hooks/useErrorModal';
 import Image from 'next/image';
 import checkmarkImage from '../../../public/images/Checkmark.png';
 
 const PaymentSuccessful: React.FC = () => {
+    const { closeModal } = useModal();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            closeModal();
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, [closeModal]);
+
     return (
         <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-40">
             <div className="bg-white p-5 rounded-lg text-center max-w-sm w-full">
