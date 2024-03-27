6af398bd4244dbcd49156028c583fc4d93c48123
NattSass67 <nattanun.sk@gmail.com>
2024-03-28 01:35:29 +0700

    fixed-navbar

--- src/frontend/src/app/my-booking/layout.tsx
+++ src/frontend/src/app/my-booking/layout.tsx
@@ -8,11 +8,11 @@ export default function UserLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-screen h-screen bg-zinc-50 fixed">
-      <NavBar />
+    <div className="w-screen h-screen bg-zinc-50 fixed ">
+      <div className="z-10 relative modal"> <NavBar /></div>
       <div className="flex h-full">
         <div className="flex flex-col flex-1">
-          <main className="flex-grow">{children}</main>
+          <main className="flex-grow relative">{children}</main>
         </div>
       </div>
     </div>
