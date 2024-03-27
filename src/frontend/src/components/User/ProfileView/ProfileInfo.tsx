b1075202ee7240cfb79094b159dc3a6846fc6223
WorkingDada <6430222521@student.chula.ac.th>
2024-03-27 16:48:44 +0700

    Fix profile photo not showing up

--- src/frontend/src/components/User/ProfileView/ProfileInfo.tsx
+++ src/frontend/src/components/User/ProfileView/ProfileInfo.tsx
@@ -9,6 +9,8 @@ import { Gallery } from "@/types/gallery";
 import { userService, photographerGalleriesService } from "@/services";
 import { PhotographerStatus } from "@/types/user";
 import { useErrorModal } from "@/hooks/useErrorModal";
+import ProfileImage from "@/components/shared/ProfileImage";
+import { profile } from "console";
 
 const Home = ({ params }: { params: { userId: string } }) => {
   const { data: session } = useSession();
@@ -42,6 +44,7 @@ const Home = ({ params }: { params: { userId: string } }) => {
         const response = await userService.getUserById(params.userId);
         if (response.data) {
           setProfilePicture(response.profile_picture_url ?? "");
+          console.log(response.data.profile_picture_key)
         }
       } catch (error) {
         console.log("error");
@@ -60,8 +63,8 @@ const Home = ({ params }: { params: { userId: string } }) => {
             <Image
               className="object-cover rounded-full"
               fill={true}
-              src={session ? profilePicture : "/images/no-picture.jpeg"}
-              alt={`session?.user.data?.username`}
+              src={session ? profilePicture === '' ? "/images/no-picture.jpeg" : profilePicture : "/images/no-picture.jpeg"}
+              alt={`Profile picture of ${session?.user?.data?.username}`}
             />
           </div>
         </div>
@@ -109,13 +112,13 @@ const Home = ({ params }: { params: { userId: string } }) => {
           </article>
           <div className="text-xl font-semibold text-amber-500">Location</div>
           <article className="text-wrap text-md text-gray-600">
-            <p>{session?.user.data?.address || "No description provided"}</p>
+            <p>{session?.user.data?.address || "No Location provided"}</p>
           </article>
           <div className="text-xl font-semibold text-amber-500">Contact</div>
           <article className="text-wrap sm:truncate text-md text-gray-600">
-            <p>{session?.user.data?.email || ""}</p>
+            <p>{session?.user.data?.email || "No contact provided"}</p>
             <p>
-              {session?.user.data?.phone_number || "No description provided"}
+              {session?.user.data?.phone_number || ""}
             </p>
           </article>
         </div>
