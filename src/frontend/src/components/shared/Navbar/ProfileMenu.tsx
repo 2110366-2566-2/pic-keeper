c942ef110d3afa799af1b1fa15372bafddbd2805
Maekin <74455990+mekintown@users.noreply.github.com>
2024-03-28 08:17:45 +0700

    Fix profile menu bug

--- src/frontend/src/components/shared/Navbar/ProfileMenu.tsx
+++ src/frontend/src/components/shared/Navbar/ProfileMenu.tsx
@@ -50,6 +50,7 @@ const ProfileMenu = () => {
   const handleCloseIssueModal = () => setIsIssueOpen(false);
 
   return (
+    <>
       <Menu as="div" className="relative ml-3">
         {({ open }) => (
           <>
@@ -59,66 +60,69 @@ const ProfileMenu = () => {
                 size={8}
               />
 
-            <span>{session?.user.data?.firstname || "Guest"}</span>
-            {open ? <MdArrowDropUp /> : <MdOutlineArrowDropDown />}
-          </Menu.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute right-0 z-50 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              {!session ? (
-                <Menu.Item>
-                  {({ active }) => (
-                    <Link
-                      href="/auth/login"
-                      className={classNames(
-                        active ? "bg-gray-100" : "",
-                        "block px-4 py-2 text-sm z-50"
-                      )}
-                    >
-                      Login / Register
-                    </Link>
-                  )}
-                </Menu.Item>
-              ) : (
-                <>
-                  <MenuItem href={`/view-profile/${session.user.data?.id}`}>
-                    Your Profile
-                  </MenuItem>
-                  {
-                    session.user.data?.is_admin === true && (
-                      <MenuItem href="/admin/dashboard/verification-tickets">Admin dashboard</MenuItem>
-                    )
-                  }
-                  <MenuItem href="/settings/edit-profile">Settings</MenuItem>
-                  <MenuItem href="/report-issues">Report issues</MenuItem>
+              <span>{session?.user.data?.firstname || "Guest"}</span>
+              {open ? <MdArrowDropUp /> : <MdOutlineArrowDropDown />}
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-50 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                {!session ? (
                   <Menu.Item>
                     {({ active }) => (
                       <Link
                         href="/auth/login"
-                        onClick={() => signOut()}
                         className={classNames(
                           active ? "bg-gray-100" : "",
                           "block px-4 py-2 text-sm"
                         )}
                       >
-                        Logout
+                        Login / Register
                       </Link>
                     )}
                   </Menu.Item>
-                </>
-              )}
-            </Menu.Items>
-          </Transition>
-        </>
-      )}
-    </Menu>
+                ) : (
+                  <>
+                    <MenuItem href={`/view-profile/${session.user.data?.id}`}>
+                      Your Profile
+                    </MenuItem>
+                    {session.user.data?.is_admin === true && (
+                      <MenuItem href="/admin/dashboard/verification-tickets">
+                        Admin dashboard
+                      </MenuItem>
+                    )}
+                    <MenuItem href="/settings/edit-profile">Settings</MenuItem>
+                    <MenuItem onClick={handleOpenIssueModal}>
+                      Report issues
+                    </MenuItem>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          onClick={() => signOut()}
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm"
+                          )}
+                        >
+                          Logout
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </>
+                )}
+              </Menu.Items>
+            </Transition>
+          </>
+        )}
+      </Menu>
+      <ReportIssue isOpen={isIssueOpen} onClose={handleCloseIssueModal} />
+    </>
   );
 };
 
