a80b63247ae5bcd4441d6d323aa1e6a0862439d8
WorkingDada <6430222521@student.chula.ac.th>
2024-04-02 20:51:19 +0700

    Added Review Component

--- src/frontend/src/components/Booking/Review.tsx
+++ src/frontend/src/components/Booking/Review.tsx
@@ -3,9 +3,17 @@ import { Review } from "@/types/review"
 const ReviewCard = ({info} : {info: Review}) => {
     return (
         <div className="w-full h-full rounded-md">
-            <div className="flex flex-col">
-                {info.customer.name}
+            <div className="flex flex-row">
+                <h1 className="">
+                    {info.customer.name}
+                </h1>
+                <div className="">
+                    {info.rating}
+                </div>
             </div>
+            <article className="text-wrap">
+                {info.review_text}
+            </article>
         </div>
     )
 }
