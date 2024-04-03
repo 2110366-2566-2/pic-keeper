faf56c76c603c7aae2ff7a98513173c170ce1fbe
WorkingDada <6430222521@student.chula.ac.th>
2024-04-03 15:22:19 +0700

    Fixed review in gallery info not showing properly

--- src/frontend/src/components/User/ProfileView/ReviewPreview.tsx
+++ src/frontend/src/components/User/ProfileView/ReviewPreview.tsx
@@ -3,16 +3,20 @@ import ReviewCard from "@/components/Booking/Review";
 
 const ReviewPreview = ({ listOfReview }: { listOfReview: Review[] }) => {
     return (
-        <div className="w-full shadow-md rounded-md">
-        <div className="text-amber-500 font-semibold text-xl p-4">Reviews</div>
-        <div className="grid grid-cols-auto-fill-400  gap-4 p-4">
-          {listOfReview &&
-            listOfReview.map((review) => (
-                <ReviewCard info={review} />
-            ))}
-        </div>
-      </div>
+      <>
+        {listOfReview && listOfReview.length > 0 && (
+          <div className="w-full shadow-md rounded-md">
+            <div className="text-amber-500 font-semibold text-xl p-4">Reviews</div>
+            <div className="grid grid-cols-auto-fill-400  gap-4 p-4">
+              {listOfReview &&
+                listOfReview.map((review) => (
+                  <ReviewCard info={review} />
+                ))}
+            </div>
+          </div>
+        )}
+      </>
     );
 };
 
-export default ReviewPreview
+export default ReviewPreview;
