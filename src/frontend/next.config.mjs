1b6092a3b114749ae6dcc39665a63de77a38d6a9
Maekin <74455990+mekintown@users.noreply.github.com>
2024-04-21 20:57:53 +0700

    Fix s3 url in frontend

--- src/frontend/next.config.mjs
+++ src/frontend/next.config.mjs
@@ -4,9 +4,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "4566",
+        protocol: "https",
+        hostname: "ls-pickeeper.ngrok.app",
       },
       {
         protocol: "https",
--- src/frontend/src/utils/s3.ts
+++ src/frontend/src/utils/s3.ts
@@ -1,6 +1,6 @@
 export const generateProfilePictureUrl = (objectKey?: string) => {
   if (objectKey) {
-    return `http://localhost:4566/profile-picture/${objectKey}`;
+    return `https://ls-pickeeper.ngrok.app/profile-picture/${objectKey}`;
   } else {
     return `/images/user-circle.svg`;
   }
