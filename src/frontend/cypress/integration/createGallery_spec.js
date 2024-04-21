f77ab7dec652b7e7030dcbb519dafd4f6f332ce0
NattSass67 <nattanun.sk@gmail.com>
2024-04-22 01:54:28 +0700

    add-gallery-cypress

--- src/frontend/cypress/integration/createGallery_spec.js
+++ src/frontend/cypress/integration/createGallery_spec.js
@@ -10,28 +10,180 @@ describe('Create Gallery Spec', () => {
     });
 
     it('TC4-1', () => {
-        // Visit the create gallery page
-        cy.url().should('eq', Cypress.config().baseUrl + '/');
+        cy.url( { timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/');
         cy.visit("/galleries/create-gallery");
-
-        // Fill in the form fields
-        cy.get('input[id="gallery"]').type("My Gallery Name");
+        cy.get('input[id="gallery"]').type("Gallery01");
         cy.get('textarea[id="description"]').type("Description of my gallery.");
-        cy.get('input[id="hours"]').type("24");
-        cy.get('input[id="location"]').type("Gallery Location");
+        cy.get('input[id="hours"]').type("5");
+        cy.get('input[id="location"]').type("Bangkok");
         cy.get('input[id="deliveryTime"]').type("5");
         cy.get('input[id="price"]').type("1000");
+        cy.get('button[type="submit"]').click(); 
+        cy.url({ timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/settings/my-galleries');
+    });
 
-        // Upload the same image 10 times
-        const fileName = '50MB.jpg'; // Adjust the file name as needed
+    it('TC4-2', () => {
+        cy.url({ timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/');
+        cy.visit("/galleries/create-gallery");
+        cy.get('input[id="gallery"]').type("Gallery01");
+        cy.get('textarea[id="description"]').type("Description of my gallery.");
+        cy.get('input[id="hours"]').type("5");
+        cy.get('input[id="location"]').type("Bangkok");
+        cy.get('input[id="deliveryTime"]').type("5");
+        cy.get('input[id="price"]').type("1000");
+        const fileName = '6MB.jpg';
+        cy.fixture(fileName).then(fileContent => {
+                cy.get('input[type="file"]').selectFile('cypress/fixtures/6MB.jpg',{ force: true });
+            }
+        );
+        cy.get('button[type="submit"]').click();
+        cy.url({ timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/settings/my-galleries');
+
+
+    });
+
+    it('TC4-3', () => {
+        cy.url({ timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/');
+        cy.visit("/galleries/create-gallery");
+        cy.get('input[id="gallery"]').type('{backspace}');
+        cy.get('textarea[id="description"]').type("Description of my gallery.");
+        cy.get('input[id="hours"]').type("2");
+        cy.get('input[id="location"]').type("Chulalongkorn");
+        cy.get('input[id="deliveryTime"]').type("2");
+        cy.get('input[id="price"]').type("2558");
+        const fileName = '6MB.jpg';
+        cy.fixture(fileName).then(fileContent => {
+                cy.get('input[type="file"]').selectFile('cypress/fixtures/6MB.jpg',{ force: true });
+            }
+        );
+        cy.get('button[type="submit"]').click();
+        cy.contains('Close', { timeout: 10000 }).should('exist'); 
+
+    });
+
+
+    it('TC4-4', () => {
+        cy.url({ timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/');
+        cy.visit("/galleries/create-gallery");
+        cy.get('input[id="gallery"]').type("Congrats RoseCP48");
+        cy.get('textarea[id="description"]').type("Description of my gallery.");
+        cy.get('input[id="hours"]').type('{backspace}');
+        cy.get('input[id="location"]').type("Satit Chula” ");
+        cy.get('input[id="deliveryTime"]').type("5");
+        cy.get('input[id="price"]').type("1412");
+        const fileName = '6MB.jpg';
+        cy.fixture(fileName).then(fileContent => {
+                cy.get('input[type="file"]').selectFile('cypress/fixtures/6MB.jpg',{ force: true });
+            }
+        );
+        cy.get('button[type="submit"]').click();
+        cy.contains('Close', { timeout: 10000 }).should('exist'); 
+
+
+    });
+
+
+    it('TC4-5', () => {
+        cy.url({ timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/');
+        cy.visit("/galleries/create-gallery");
+        cy.get('input[id="gallery"]').type("Gallery 789");
+        cy.get('textarea[id="description"]').type("Description of my gallery.");
+        cy.get('input[id="hours"]').type("5");
+        cy.get('input[id="location"]').type('{backspace}');
+        cy.get('input[id="deliveryTime"]').type("5");
+        cy.get('input[id="price"]').type("2000");
+        const fileName = '6MB.jpg';
+        cy.fixture(fileName).then(fileContent => {
+                cy.get('input[type="file"]').selectFile('cypress/fixtures/6MB.jpg',{ force: true });
+            }
+        );
+        cy.get('button[type="submit"]').click();
+        cy.contains('Close', { timeout: 10000 }).should('exist'); 
+
+
+    });
+
+
+    it('TC4-6', () => {
+        cy.url({ timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/');
+        cy.visit("/galleries/create-gallery");
+        cy.get('input[id="gallery"]').type("Gallery 789");
+        cy.get('textarea[id="description"]').type("Description of my gallery.");
+        cy.get('input[id="hours"]').type("5");
+        cy.get('input[id="location"]').type("Bangkok");
+        cy.get('input[id="deliveryTime"]').type('{backspace}');
+        cy.get('input[id="price"]').type("2000");
+        const fileName = '6MB.jpg';
+        cy.fixture(fileName).then(fileContent => {
+                cy.get('input[type="file"]').selectFile('cypress/fixtures/6MB.jpg',{ force: true });
+            }
+        );
+        cy.get('button[type="submit"]').click();
+        cy.contains('Close', { timeout: 10000 }).should('exist'); 
+
+    });
+
+
+    it('TC4-7', () => {
+        cy.url({ timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/');
+        cy.visit("/galleries/create-gallery");
+        cy.get('input[id="gallery"]').type("Gallery01");
+        cy.get('textarea[id="description"]').type("Description of my gallery.");
+        cy.get('input[id="hours"]').type("5");
+        cy.get('input[id="location"]').type("Bangkok");
+        cy.get('input[id="deliveryTime"]').type('5');
+        cy.get('input[id="price"]').type('{backspace}');
+        const fileName = '6MB.jpg';
+        cy.fixture(fileName).then(fileContent => {
+                cy.get('input[type="file"]').selectFile('cypress/fixtures/6MB.jpg',{ force: true });
+            }
+        );
+        cy.get('button[type="submit"]').click();
+        cy.contains('Close', { timeout: 10000 }).should('exist'); 
+
+
+    });
+
+
+
+    it('TC4-8', () => {
+        cy.url({ timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/');
+        cy.visit("/galleries/create-gallery");
+        cy.get('input[id="gallery"]').type("Gallery01");
+        cy.get('textarea[id="description"]').type("Description of my gallery.");
+        cy.get('input[id="hours"]').type("5");
+        cy.get('input[id="location"]').type("Bangkok");
+        cy.get('input[id="deliveryTime"]').type("5");
+        cy.get('input[id="price"]').type("1000");
+        const fileName = '50MB.jpg';
         cy.fixture(fileName).then(fileContent => {
                 cy.get('input[type="file"]').selectFile('cypress/fixtures/50MB.jpg',{ force: true });
             }
         );
-
-        // Click the submit button to save the gallery
         cy.get('button[type="submit"]').click();
+        cy.contains('Close', { timeout: 10000 }).should('exist'); 
 
-        // Add further assertions as needed to verify the behavior after submitting the form
+    });
+
+
+
+    it('TC4-9', () => {
+        cy.url({ timeout: 10000 }).should('eq', Cypress.config().baseUrl + '/');
+        cy.visit("/galleries/create-gallery");
+        cy.get('input[id="gallery"]').type("Gallery01");
+        cy.get('textarea[id="description"]').type("Description of my gallery.");
+        cy.get('input[id="hours"]').type("5");
+        cy.get('input[id="location"]').type("Bangkok");
+        cy.get('input[id="deliveryTime"]').type("5");
+        cy.get('input[id="price"]').type("1000");
+        const fileName = 'file.pdf';
+        cy.fixture(fileName).then(fileContent => {
+                cy.get('input[type="file"]').selectFile('cypress/fixtures/file.pdf',{ force: true });
+            }
+        );
+        cy.get('button[type="submit"]').click();
+        cy.contains('Close', { timeout: 10000 }).should('exist'); 
+
+
     });
 });
